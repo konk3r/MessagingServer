1e597b448271183051f023d323697da9a3b021db
Gabriel Spencer <yao@acm.org>
2012-09-18 21:37:43 -0500

    Forgot to add some files with last update

---
+++ config/initializers/gcm_on_rails.rb
@@ -0,0 +1,4 @@
+configatron.gcm_on_rails.api_url = 'https://android.googleapis.com/gcm/send'
+configatron.gcm_on_rails.api_key = 'AIzaSyBUU07B9fhSq-WNLbyYYDakhILDonM-OzE'
+configatron.gcm_on_rails.app_name = 'com.warmice.android.videomessaging'
+configatron.gcm_on_rails.delivery_format = 'json'
