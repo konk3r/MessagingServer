da380e003b80afbe40c6cd5c42947d643bc86e81
Gabriel Spencer <yao@acm.org>
2012-09-19 02:22:44 -0500

    more testing

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -8,7 +8,11 @@ class DevicesController < ApplicationController
   
   def destroy
     @current_user.remove_device(params[:device_id])
-    render :json => @current_user
+    if (@current_user.device_id == nil)
+      render :json => {:status => 'device removed'}
+    else
+      render :status => :bad_request :json => {:status => 'failed to remove device'}
+    end
   end
 
 end
