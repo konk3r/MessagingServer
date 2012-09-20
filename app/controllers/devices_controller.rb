4c25099cf21e29652c0cbd29a2c30da8528ba41f
Gabriel Spencer <yao@acm.org>
2012-09-19 21:11:08 -0500

    Fixed error in DevicesController, it was calling add_device and remove_device which had been changed to exclamation point versions

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -2,7 +2,7 @@ class DevicesController < ApplicationController
   before_filter :authenticate_user
   
   def create
-    @current_user.add_device(params[:device_id])
+    @current_user.add_device!(params[:device_id])
     render :json => @current_user
   end
   
@@ -15,7 +15,7 @@ class DevicesController < ApplicationController
   end
   
   def destroy
-    @current_user.remove_device(params[:device_id])
+    @current_user.remove_device!(params[:device_id])
     render :json => {:status => 'device removed'}
   end
 
